c566f1f84d8a08cad4cf13c983e81e28867f833e
kushalkk752 <kushalkk555@gmail.com>
2025-01-22 10:18:32 +0530

    second commit

--- src/main/java/com/lms/controller/LoginController.java
+++ src/main/java/com/lms/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.lms.controller;
 
 import com.lms.payload.LoginDto;
+import com.lms.payload.TokenDto;
 import com.lms.service.LoginService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +20,19 @@ public class LoginController {
         this.loginService = loginService;
     }
 
-    public ResponseEntity<LoginDto> LoginUser(
+    @PostMapping("/login")
+    public ResponseEntity<?> LoginUser(
             @RequestBody LoginDto dto
     ){
         String token = loginService.loginUser(dto);
-        if()
+        if(token != null){
+            TokenDto tokenDto = new TokenDto();
+            tokenDto.setToken(token);
+            tokenDto.setType("JWT");
+            return new ResponseEntity<>(tokenDto, HttpStatus.CREATED);
+        }
+        else {
+            return new ResponseEntity<>("Invalid username/password", HttpStatus.FORBIDDEN);
+        }
     }
 }
